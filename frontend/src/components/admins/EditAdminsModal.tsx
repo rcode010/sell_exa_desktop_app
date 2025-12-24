26bd57a86ba490623f4c520d62288b2c6a3b327d
Raman <ramanazad881@gmail.com>
2025-12-24 15:15:32 +0300

    Create Edit Admin Modal

--- frontend/src/components/admins/EditAdminsModal.tsx
+++ frontend/src/components/admins/EditAdminsModal.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { X, Building2, Package, Trash2 } from "lucide-react";
+import { X, Trash2, Loader, Key } from "lucide-react";
 import { User } from "../../types/user";
 
 const EditAdminsModal = ({
@@ -9,17 +9,18 @@ const EditAdminsModal = ({
   admin: User;
   onClose: () => void;
 }) => {
-  const [formData, setFormData] = useState<User>({
-    _id: "",
-    firstName: "",
-    lastName: "",
-    phoneNo: "",
-    role: "admin",
+  const [formData, setFormData] = useState({
+    password: "",
+    confirmPassword: "",
   });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [showPasswordSection, setShowPasswordSection] = useState(false);
 
   // Handle update
   const handleUpdate = async (e: React.FormEvent) => {
     e.preventDefault();
+    console.log(formData.password)
+    console.log("handle update");
 
     // const success = await updateCompany(formDataToSend, company._id as number);
 
@@ -29,7 +30,9 @@ const EditAdminsModal = ({
   };
 
   // Handle delete
-  const handleDelete = async () => {
+  const handleDelete = async (e: React.FormEvent) => {
+
+    console.log("handle delete");
     // if (
     //   globalThis.confirm(`Are you sure you want to delete ${company.name}?`)
     // ) {
@@ -46,9 +49,9 @@ const EditAdminsModal = ({
         {/* Header */}
         <div className="flex items-center justify-between p-6 border-b border-gray-200">
           <div>
-            <h2 className="text-2xl font-bold text-gray-900">Edit Company</h2>
+            <h2 className="text-2xl font-bold text-gray-900">Edit Admin</h2>
             <p className="text-sm text-gray-500 mt-1">
-              Update company information
+              Update admin information
             </p>
           </div>
           <button
@@ -61,47 +64,108 @@ const EditAdminsModal = ({
 
         {/* Body */}
         <div className="flex-1 overflow-y-auto p-6 space-y-6">
-          {/* Stats */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div className="flex items-start gap-3 p-4 bg-gray-50 rounded-lg">
-              <div className="p-2 bg-blue-100 rounded-lg">
-                <Package className="w-5 h-5 text-blue-600" />
-              </div>
-              <div>
-                <p className="text-sm text-gray-500 font-medium">
-                  Total Products
-                </p>
-                <p className="text-lg font-semibold text-gray-900">
-                  {admin.firstName}
-                </p>
-              </div>
-            </div>
+          <div className="flex flex-col">
+            <label
+              htmlFor="fullName"
+              className="text-sm font-medium text-gray-900 mb-2"
+            >
+              Full Name
+            </label>
+            <input
+              id="fullName"
+              type="text"
+              value={admin.firstName + " " + admin.lastName}
+              readOnly
+              placeholder="Yazen adnnan"
+              className="w-full px-4  placeholder:text-gray-400 py-3 border border-gray-200 rounded-md bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
           </div>
 
-          {/* Form */}
           <form onSubmit={handleUpdate} className="space-y-5">
             {/* Name */}
-            <div>
-              <label className="block text-sm font-medium text-gray-700 mb-2">
-                Company Name *
+            <div className="flex flex-col">
+              <label
+                htmlFor="phone"
+                className="text-sm font-medium text-gray-900 mb-2"
+              >
+                Phone Number
               </label>
-              <div className="relative">
-                <Building2 className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400" />
-                <input
-                  type="text"
-                  value={formData.firstName}
-                  onChange={(e) =>
-                    setFormData({
-                      ...formData,
-                      firstName: e.target.value,
-                    })
-                  }
-                  className="w-full pl-10 pr-4 py-3 bg-gray-50 border border-gray-200 rounded-lg focus:ring-2 focus:ring-blue-500"
-                  required
-                />
-              </div>
+              <input
+                id="phone"
+                type="tel"
+                value={admin.phoneNo}
+                disabled={isSubmitting}
+                readOnly
+                placeholder="07xxxxxxxxx"
+                className="px-4  placeholder:text-gray-400 py-3 border border-gray-200 rounded-md bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
             </div>
-
+            <label className="block text-sm font-medium text-gray-700 mb-2">
+              Role *
+            </label>
+            <div className="relative">
+              <input
+                readOnly
+                value={admin.role}
+                disabled={isSubmitting}
+                className="w-full pl-5 pr-4 py-3 bg-gray-50 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              ></input>
+            </div>
+            <button
+            type="button"
+              onClick={() => setShowPasswordSection((p)=>!p)}
+              className="flex items-center gap-2 px-4 py-2 text-sm cursor-pointer text-blue-600 hover:bg-blue-50 rounded-lg transition-colors"
+            >
+              <Key className="w-4 h-4" />
+              Change Password
+            </button>
+            {showPasswordSection && (
+              <>
+                <div className="bg-blue-100 rounded-lg  w-full max-w-2xl max-h-[90vh] p-7 overflow-hidden flex flex-col space-y-6">
+                  <div className="flex flex-col">
+                    <label
+                      htmlFor="password"
+                      className="text-sm font-medium text-gray-900 mb-2"
+                    >
+                      New password
+                    </label>
+                    <input
+                      id="password"
+                      type="password"
+                      value={formData.password}
+                      disabled={isSubmitting}
+                      onChange={(e) => {
+                        setFormData({ ...formData, password: e.target.value });
+                      }}
+                      placeholder="Enter new Password"
+                      className="px-4  placeholder:text-gray-400 py-3 border border-gray-200 rounded-md bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label
+                      htmlFor="confirmationPassword"
+                      className="text-sm font-medium text-gray-900 mb-2"
+                    >
+                      Confirmation Password
+                    </label>
+                    <input
+                      id="confirmationPassword"
+                      type="password"
+                      value={formData.confirmPassword}
+                      disabled={isSubmitting}
+                      onChange={(e) => {
+                        setFormData({
+                          ...formData,
+                          confirmPassword: e.target.value,
+                        });
+                      }}
+                      placeholder="Re-enter password"
+                      className="px-4  placeholder:text-gray-400 py-3 border border-gray-200 rounded-md bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                  </div>
+                </div>
+              </>
+            )}
             {/* Delete */}
             <div className="pt-6 border-t border-gray-200">
               <div className="bg-red-50 border border-red-200 rounded-lg p-4">
@@ -111,17 +175,22 @@ const EditAdminsModal = ({
                   </div>
                   <div>
                     <h4 className="text-sm font-semibold text-red-900">
-                      Delete Company
+                      Delete Admin
                     </h4>
                     <p className="text-sm text-red-700 mt-1">
                       This action cannot be undone.
                     </p>
                     <button
+                      disabled={isSubmitting}
                       type="button"
                       onClick={handleDelete}
-                      className="mt-3 px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700"
+                      className="cursor-pointer mt-3 px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700"
                     >
-                      Delete Company
+                      {isSubmitting ? (
+                        <Loader className="animate-spin" />
+                      ) : (
+                        "Delete Admin"
+                      )}
                     </button>
                   </div>
                 </div>
@@ -133,15 +202,19 @@ const EditAdminsModal = ({
               <button
                 type="button"
                 onClick={onClose}
-                className="px-6 py-3 border border-gray-300 rounded-lg hover:bg-gray-100"
+                className="px-6 py-3 border cursor-pointer border-gray-300 rounded-lg hover:bg-gray-100"
               >
                 Cancel
               </button>
               <button
                 type="submit"
-                className="px-6 py-3 bg-black text-white rounded-lg hover:bg-gray-800"
+                className="px-6 py-3 cursor-pointer bg-black text-white rounded-lg hover:bg-gray-800"
               >
-                Save Changes
+                {isSubmitting ? (
+                  <Loader className="animate-spin" />
+                ) : (
+                  "Save changes"
+                )}
               </button>
             </div>
           </form>
